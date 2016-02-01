e68f06690e8aab97cf39c5a535745d2210635242
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2016-02-01 13:50:25 +0200

    Wrap the code in a factory function and add a UMD header to call it.

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-'use strict';
-module.exports = function (arr, predicate, ctx) {
-	if (typeof Array.prototype.findIndex === 'function') {
-		return arr.findIndex(predicate, ctx);
+(function (root, factory) {
+	'use strict';
+
+	var define;
+
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+	} else if (typeof module === 'object' && module.exports) {
+		module.exports = factory();
+	} else {
+		root.shimArrayFindIndex = factory();
 	}
+})(this, factory);
+function factory() {
+	'use strict';
 
-	if (typeof predicate !== 'function') {
-		throw new TypeError('predicate must be a function');
-	}
+	function findIndex(arr, predicate, ctx) {
+		if (typeof Array.prototype.findIndex === 'function') {
+			return arr.findIndex(predicate, ctx);
+		}
 
-	var list = Object(arr);
-	var len = list.length;
+		if (typeof predicate !== 'function') {
+			throw new TypeError('predicate must be a function');
+		}
 
-	if (len === 0) {
+		var list = Object(arr);
+		var len = list.length;
+
+		if (len === 0) {
+			return -1;
+		}
+
+		for (var i = 0; i < len; i++) {
+			if (predicate.call(ctx, list[i], i, list)) {
+				return i;
+			}
+		}
+
 		return -1;
 	}
 
-	for (var i = 0; i < len; i++) {
-		if (predicate.call(ctx, list[i], i, list)) {
-			return i;
-		}
-	}
-
-	return -1;
-};
+	return findIndex;
+}
